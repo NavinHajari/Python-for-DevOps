4a69cdbfd6d6ca81a552e84f3c1e53ce3d312d98
NavinHajari <navinsingh_hajari@yahoo.co.in>
2026-04-28 08:39:37 +0530

    api_weather.py added

---
+++ Day02/api_weather.py
@@ -0,0 +1,23 @@
+import requests
+class API():
+
+    def api_check(self):
+        url = 'https://jsonplaceholder.typeicode.com/posts'
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("API is working")
+        else:
+            print("API is not working")
+    
+    def api_weather(self,city):
+        url = f'https://wttr.in/{city}'
+        response = requests.get(url)
+        print(response.text)
+
+api_a = API()
+api_a.api_check()
+api_a.api_weather('New York')
+
+
+
+
