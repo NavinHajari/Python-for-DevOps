57936339b97e042419ecf45395e426f539749611
NavinHajari <navinsingh_hajari@yahoo.co.in>
2026-05-06 12:10:18 +0530

    updated app.y

--- Day03/monitoring_app/app.py
+++ Day03/monitoring_app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import Response
+import psutil
 
 app = Flask(__name__)
-@app.route("/")
 
-def hello():
-    return Response({"Server is up & running"})
+#@app.route("/")
+#def hello():
+#    return Response({"Server is up & running"})
+
+@app.route("/cpu")
+def monitor_cpu():
+    cpu_percent = psutil.cpu_percent()
+    return Response({f"The CPU utilization is {cpu_percent}%"})
 
 if __name__=='__main__':
     print("This will run only if I execute app.py")
